32be198a420d45037f397e80b937746d623583f8
Guilherme Ulchak <guilherme.ulchak@gmail.com>
2021-07-19 21:59:59 -0300

    script para funcionamento da bateria

---
+++ script.js
@@ -0,0 +1,43 @@
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLowerCase());
+});
+
+document.querySelector(".composer button").addEventListener("click", () => {
+  const song = document.querySelector("#input").value;
+
+  if(song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+  };
+});
+
+function playSound(sound) {
+  const audioElement = document.querySelector(`#s_${sound}`);
+  const keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+  if(audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  };
+
+  if(keyElement) { 
+    keyElement.classList.add("active");
+
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 300);
+};
+};
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for(let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+
+    wait += 300;
+
+  };
+};
